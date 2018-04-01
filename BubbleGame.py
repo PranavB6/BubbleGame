9dc18bd26c30efc241f6eb37d16aa219deb6004a
ChrisChrisLoLo <ChrisChrisLoLo>
2018-04-01 14:48:58 -0600

    quick fix

--- BubbleGame.py
+++ BubbleGame.py
@@ -1,9 +1,10 @@
-import time, random, pygame
+import time, random
+import pygame as pg
 from constants import *
 pygame.init()
 
 #create display
-Display = pygame.display.set_mode((DISP_W,DISP_H))
+display = pygame.display.set_mode((DISP_W,DISP_H))
 
 # Change title of window
 pygame.display.set_caption(CAPTION)
@@ -11,17 +12,24 @@ pygame.display.set_caption(CAPTION)
 # Game specific clock
 clock = pygame.time.Clock()
 
-
+def drawBackground():
+	display.fill(LIGHT_GRAY)
+	#pygame.draw.rect(display,BLACK,10)
+	#pygame.draw.rect()
 def main():
 	print('program start')
 	while True:
+		drawBackground()
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				quit()
 
 		pygame.display.update()
+		
 		clock.tick(60)
+
 
 
 	return
--- constants.py
+++ constants.py
@@ -1,13 +1,18 @@
+
 DISP_W = 900
 DISP_H = 700
 CAPTION = 'Bubble Trouble but worse'
 
 # colours
 BLACK = (0, 0, 0 )
+LIGHT_GRAY = (122, 122, 122)
 WHITE = (255, 255, 255)
 RED   = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE  = (0, 0, 255)
 
-#shape constants
-RECT_WIDTH = 30
+#Game environment constants
+WALL_WIDTH = 30
+BOUND_L = WALL_WIDTH
+BOUND_R = DISP_H - WALL_WIDTH
+WALL_RECT = pygame
