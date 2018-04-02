dc322f5ac169eb7ef98f2feec8b862bae8a7a531
PranavB6 <b.pranav.k@gmail.com>
2018-04-02 13:42:06 -0600

    Added spinning gun

--- BubbleGame.py
+++ BubbleGame.py
@@ -1,10 +1,13 @@
 import time, random
 import pygame as pg
-from constants import *
 pg.init()
+from constants import *
+
+import math
 
 #create display
 display = pg.display.set_mode((DISP_W,DISP_H))
+display_rect = display.get_rect()
 
 # Change title of window
 pg.display.set_caption(CAPTION)
@@ -13,11 +16,36 @@ pg.display.set_caption(CAPTION)
 clock = pg.time.Clock()
 
 def drawBackground():
-	display.fill(LIGHT_GRAY)
-	pg.draw.rect(display,DARK_GRAY,WALL_RECT_L)
-	pg.draw.rect(display,DARK_GRAY,WALL_RECT_R)
+	display.fill(WHITESMOKE)
+	pg.draw.rect(display,DARK_GRAY, WALL_RECT_L)
+	pg.draw.rect(display,DARK_GRAY, WALL_RECT_R)
+
+def getImg():
+	gun_og = pg.image.load('gun.png').convert()
+	gun_og_rect = gun_og.get_rect()
+
+	gun_og_rect_w = gun_og_rect[2]
+	gun_og_rect_h = gun_og_rect[3]
+
+
+	sf = 0.25
+	gun_og = pg.transform.scale(gun_og, (int(gun_og_rect_w * sf), int(gun_og_rect_h * sf)))
+
+	return gun_og
+
 def main():
+
 	print('program start')
+
+	# ------------------------------
+
+
+	image_og = getImg()
+	image_og_rect = image_og.get_rect( center = display_rect.center) # center = (display_rect.center)
+	angle = 0
+
+	# ---------------------
+
 	while True:
 		drawBackground()
 
@@ -26,6 +54,11 @@ def main():
 				pg.quit()
 				quit()
 
+		angle += 1
+		gun = pg.transform.rotate(image_og, angle)
+		gun_rect = gun.get_rect( center = (450,650))
+
+		display.blit(gun, gun_rect)
 		pg.display.update()
 		
 		clock.tick(60)
