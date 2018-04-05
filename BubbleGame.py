9c8ae62f712aa970b6e835c4457f775d43dae28b
PranavB6 <b.pranav.k@gmail.com>
2018-04-04 21:51:52 -0600

    The gun can move!

--- BubbleGame.py
+++ BubbleGame.py
@@ -2,12 +2,10 @@ import time, random
 import pygame as pg
 pg.init()
 from constants import *
-
+from shooter_file import Shooter
 import math
 
-#create display
-display = pg.display.set_mode((DISP_W,DISP_H))
-display_rect = display.get_rect()
+
 
 # Change title of window
 pg.display.set_caption(CAPTION)
@@ -20,40 +18,10 @@ def drawBackground():
 	pg.draw.rect(display,DARK_GRAY, WALL_RECT_L)
 	pg.draw.rect(display,DARK_GRAY, WALL_RECT_R)
 
-def getGun():
-
-	# Load image
-	gun = pg.image.load('gun.png').convert_alpha()
-
-	# Get width and height
-	gun_rect = gun.get_rect()
-	gun_w = gun_rect[2]
-	gun_h = gun_rect[3]
-
-	# Scale image
-	sf = 0.25
-	gun = pg.transform.scale(gun, (int(gun_w * sf), int(gun_h * sf)))
-
-	# Scale Image
-	return gun
 
 
-def putGunInBox(gun):
 
-	# Get gun dimensions
-	gun_rect = gun.get_rect()
-	gun_w = gun_rect[2]
-	gun_h = gun_rect[3]
 
-	# Make a box to put gun in
-	# Surface((width, height), flags=0, depth=0, masks=None) -> Surface
-	gun_box = pg.Surface((gun_w, gun_h*2))
-	gun_box.fill(WHITESMOKE)
-
-	# Put gun in box
-	gun_box.blit(gun, gun_rect)
-
-	return gun_box
 
 
 def main():
@@ -61,9 +29,11 @@ def main():
 	print('program start')
 
 	# ------------------------------
-	gun = getGun()
-	gun_box = putGunInBox(gun)
-	angle = 0
+	# Initialize the shooter
+	gun = Shooter(center = BOTTOM_CENTER)
+	# put it in a box
+	gun.putInBox()
+	
 	# ---------------------
 
 	while True:
@@ -75,15 +45,17 @@ def main():
 				pg.quit()
 				quit()
 
-		angle += 1
+			if event.type == pg.MOUSEMOTION:
+				mouse_pos = pg.mouse.get_pos()
+				
+		# Rotate and draw gun
+		gun.rotate(mouse_pos)
+
 		
-		# Note: Don't change the original image, you will slowly lose information
-		# Rotate the box
-		rotated_box = pg.transform.rotate(gun_box, angle)
 
 		# Display the box on the screen
 		# Note: center is the pivot point
-		display.blit(rotated_box, rotated_box.get_rect( center = display_rect.center))
+		
 
 		pg.display.update()
 		
--- constants.py
+++ constants.py
@@ -1,6 +1,8 @@
 import pygame as pg
+from math import pi
 DISP_W = 900
 DISP_H = 700
+BOTTOM_CENTER = (450,700)
 CAPTION = 'Bubble Trouble but worse'
 
 # colours
@@ -12,6 +14,7 @@ RED   = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE  = (0, 0, 255)
 WHITESMOKE = (245,245,245)
+BG_COLOUR = WHITESMOKE
 
 #Game environment constants
 WALL_WIDTH = 120
@@ -19,3 +22,13 @@ WALL_BOUND_L = WALL_WIDTH
 WALL_BOUND_R = DISP_W - WALL_WIDTH
 WALL_RECT_L = pg.Rect(0,0,WALL_WIDTH,DISP_H)
 WALL_RECT_R = pg.Rect(WALL_BOUND_R,0,WALL_WIDTH,DISP_H)
+
+
+
+#create display
+display = pg.display.set_mode((DISP_W,DISP_H))
+display_rect = display.get_rect()
+
+
+ANGLE_MAX = 180 - 15
+ANGLE_MIN = 0 + 15
