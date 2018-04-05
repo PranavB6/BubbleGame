f04c47f7715433ffa80880d183f7bdc5c4e897f8
PranavB6 <b.pranav.k@gmail.com>
2018-04-05 10:44:19 -0600

    Made changes

--- bubble.py
+++
@@ -1,8 +0,0 @@
-class bubble():
-	def __init__(self):
-		self.diameter = BALL_DIAMETER
-		self.color = color
-		#NOTE: MIGHT WANT TO CHANGE X AND Y TO SINGLE POINT
-		self.x_pos = x_pos
-		self.y_pos = y_pos
-	def draw(self):
