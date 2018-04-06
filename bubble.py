b9a57e74d806e4772294b871aaa01691aa8bc44d
PranavB6 <b.pranav.k@gmail.com>
2018-04-06 17:44:13 -0600

    SHIT FINALLY WORKS

--- bubble.py
+++ bubble.py
@@ -20,12 +20,29 @@ class gridBubble(bubble):
 		# self.id = id_Count
 		# id_Count += 1
 
+		self.L = None
+		self.R = None
+		self.UL = None
+		self.UR = None
+		self.DL = None
+		self.DR = None 
+
 	def calcPos(self):
 		x = (self.col * ((ROOM_WIDTH-BUBBLE_RADIUS) / (GRID_COLS)))+WALL_BOUND_L+BUBBLE_RADIUS
 		if self.row%2 == 0:
 			x+=BUBBLE_RADIUS
 		y = BUBBLE_RADIUS + self.row*BUBBLE_RADIUS*2
 		self.pos = (x,y)
+
+	def getNeighbs(self):
+
+		neighbs = [self.L, self.R, self.UL, self.UR, self.DL, self.DR]
+		alive = []
+
+		for neighb in neighbs:
+			if neighb: alive.append(neighb)
+
+		return alive
 
 class bullet(bubble):
 	def __init__(self,color,pos,angle):
@@ -46,15 +63,15 @@ class bullet(bubble):
 		self.draw()
 	def getGridPos(self,grid):
 		for i in range(grid.rows):	
-			for j in range(grid.cols):
+			for j in range(grid._cols):
 				#Check if balls x is within a given slot
 				if not grid.grid[i][j].exists:
-					print("SFDFDS")
+					# print("SFDFDS")
 					#TODO: COLLISIONS ARE NOT SPOT ON. WILL FAIL IF YOU FIRE HEAD ON.
 					if grid.grid[i][j].pos[0]-BUBBLE_RADIUS<=self.pos[0]<=grid.grid[i][j].pos[0]+BUBBLE_RADIUS:
-						print("PASS 1")
+						# print("PASS 1")
 						if grid.grid[i][j].pos[1]-BUBBLE_RADIUS<self.pos[1]<grid.grid[i][j].pos[1]+BUBBLE_RADIUS:
-							print("HIT")
+							# print("HIT")
 							print(str(i)+","+str(j))
 							grid.grid[i][j].color=self.color
 							grid.grid[i][j].exists=True
--- gameObj.py
+++ gameObj.py
@@ -11,22 +11,29 @@ class game():
 class gameGrid():
 	def __init__(self):
 		self.rows = GRID_ROWS
-		self.cols = GRID_COLS
-		self.grid = [[0 for x in range(GRID_COLS)] for y in range(GRID_ROWS)]
-		for i in range(GRID_ROWS):
-			for j in range(GRID_COLS):
-				self.grid[i][j] = gridBubble(GREEN,i,j,True)
+		self._cols = GRID_COLS
+		self.grid = [[0 for x in range(self._cols)] for y in range(self.rows)]
+		for i in range(self.rows):
+			for j in range(self._cols):
+
+				if j % 2:
+					self.grid[i][j] = gridBubble(GREEN,i,j,True)
+				else:
+					self.grid[i][j] = gridBubble(RED,i,j,True)
 				self.grid[i][j].draw()
 		self.appendBottom()
 		#self.graph = self.makeGraph()
+		self.initNeighb()
+		self.test()
+
 	def draw(self):
 		for i in range(self.rows):
-			for j in range(GRID_COLS):
+			for j in range(self._cols):
 				if self.grid[i][j]:
 					self.grid[i][j].draw()
 	def check(self,bullet_pos,bullet):
 		for i in range(self.rows):
-			for j in range(GRID_COLS):
+			for j in range(self._cols):
 				# print(str(i)+","+str(j))
 				gridElement = self.grid[i][j]
 				if gridElement:
@@ -39,7 +46,7 @@ class gameGrid():
 						else:
 							pass
 		#Check if the bottom row is completely null, if not, add a null row
-		for j in range(GRID_COLS):
+		for j in range(self._cols):
 			if self.grid[self.rows-1][j].exists:
 				self.appendBottom()
 
@@ -49,10 +56,96 @@ class gameGrid():
 			#self.grid[i][j].color = WHITE
 	def appendBottom(self):
 		row = []
-		for j in range(GRID_COLS):
+		for j in range(self._cols):
 			row.append(gridBubble(BLACK,self.rows,j,False))
 		self.grid.append(row)	
 		self.rows += 1
+
+	def initNeighb(self):
+
+		for row in range(self.rows):
+			for col in range(self._cols):
+
+				if row % 2 != 0:
+					if col > 0:
+						self.grid[row][col].L = (row, col - 1)
+
+					if col < (self._cols - 1):
+						self.grid[row][col].R = (row,col + 1)
+
+					if row > 0:
+
+						self.grid[row][col].UR = (row - 1,col)
+
+						if col > 0:
+							self.grid[row][col].UL = (row - 1,col - 1)
+
+					if row < (self.rows - 1):
+						self.grid[row][col].DR = (row + 1, col)
+
+						if col > 0:
+							self.grid[row][col].DL = (row + 1,col - 1)					
+							
+
+
+				if row % 2 == 0:
+					if col > 0:
+						self.grid[row][col].L = (row, col - 1)
+
+					if col < (self._cols - 1):
+						self.grid[row][col].R = (row,col + 1)
+
+					if row > 0:
+						self.grid[row][col].UL = (row - 1,col)
+
+						if col < (self._cols - 1):
+							self.grid[row][col].UR = (row - 1,col + 1)
+
+					if row < (self.rows - 1):
+						self.grid[row][col].DL = (row + 1,col)
+
+						if col < (self._cols - 1):
+							self.grid[row][col].DR = (row + 1, col + 1)
+
+		return
+
+	def test(self):
+
+		# for row in range(self.rows):
+		# 	for col in range(self._cols):
+		# 		print("(row, col): ({} {})".format(row, col))
+		# 		print(self.grid[row][col].getNeighbs())
+
+		reached = self.search(self.grid[0][0])
+
+		
+
+		for bubble in reached:
+			print('(row,col): ({},{})'.format(bubble.row, bubble.col))
+
+		return
+
+	def search(self, bubble, reached = None):
+
+		print(bubble)
+		
+
+		if reached == None: reached = []
+
+		if bubble in reached: return
+
+		reached.append(bubble)
+
+		for neighb in bubble.getNeighbs():
+			new_bubble = self.grid[neighb[0]][neighb[1]]
+
+			if new_bubble.exists:
+				if new_bubble.color == bubble.color:
+					self.search(new_bubble, reached)
+
+		return reached
+
+
 
 def drawBackground():
 	display.fill(BG_COLOUR)
